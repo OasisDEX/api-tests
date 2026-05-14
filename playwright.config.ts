0d29347a7a0c1c0767ded4c59a89855bd5597fd2
juan-langa <juan@oazoapps.com>
2026-05-14 09:45:12 +0200

    Seting up test framework for SDK API tests

---
+++ playwright.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+	testDir: './tests',
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
+	retries: process.env.CI ? 2 : 0,
+	workers: process.env.CI ? 1 : undefined,
+	reporter: 'html',
+	use: {
+		trace: 'on-first-retry',
+		// Base URL to use in actions like `await request.get('/')`
+		// baseURL: 'https://api.example.com',
+	},
+	projects: [
+		// {
+		// 	name: 'api',
+		// 	use: {},
+		// },
+
+		{
+			name: 'sdk',
+			testDir: './tests/sdk',
+			use: {},
+		},
+	],
+});
