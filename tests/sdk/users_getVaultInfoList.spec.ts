06e99b5949d8ce4d81defd9260419a80399fb8b0
juan-langa <juan@oazoapps.com>
2026-05-18 14:31:15 +0200

    SDK API tests for 'getVaultInfo' and 'getVaultInfoList' - Positive and Negative scenarios

--- tests/sdk/users_getVaultInfoList.spec.ts
+++ tests/sdk/users_getVaultInfoList.spec.ts
@@ -56,3 +56,92 @@ test.describe('getVaultInfoList', () => {
 		expect(response).toMatchObject(getVaultInfoListSonic);
 	});
 });
+
+test.describe.only('getVaultInfoList - Negative scenarios ', () => {
+	test('"chainId" undefined', async () => {
+		try {
+			await sdk.armada.users.getVaultInfoList({
+				// @ts-ignore
+				chainId: undefined,
+			});
+
+			// If no error is thrown, the test should fail
+			throw new Error('Should have thrown a TRPCClientError');
+		} catch (error: any) {
+			// Assert the error type
+			expect(error.name).toBe('TRPCClientError');
+
+			// Assert the TRPC error data
+			expect(error.data).toMatchObject({
+				code: 'BAD_REQUEST',
+				httpStatus: 400,
+			});
+
+			// Assert the content of the error message (Zod validation error)
+			const message = JSON.parse(error.message);
+			expect(message[0]).toMatchObject({
+				code: 'custom',
+				message: 'Invalid input',
+				path: ['chainId'],
+			});
+		}
+	});
+
+	test('"chainId" out of range', async () => {
+		try {
+			await sdk.armada.users.getVaultInfoList({
+				// @ts-ignore
+				chainId: 5555,
+			});
+
+			// If no error is thrown, the test should fail
+			throw new Error('Should have thrown a TRPCClientError');
+		} catch (error: any) {
+			// Assert the error type
+			expect(error.name).toBe('TRPCClientError');
+
+			// Assert the TRPC error data
+			expect(error.data).toMatchObject({
+				code: 'BAD_REQUEST',
+				httpStatus: 400,
+			});
+
+			// Assert the content of the error message (Zod validation error)
+			const message = JSON.parse(error.message);
+			expect(message[0]).toMatchObject({
+				code: 'custom',
+				message: 'Invalid input',
+				path: ['chainId'],
+			});
+		}
+	});
+
+	test('"chainId" type as a string', async () => {
+		try {
+			await sdk.armada.users.getVaultInfoList({
+				// @ts-ignore
+				chainId: '1',
+			});
+
+			// If no error is thrown, the test should fail
+			throw new Error('Should have thrown a TRPCClientError');
+		} catch (error: any) {
+			// Assert the error type
+			expect(error.name).toBe('TRPCClientError');
+
+			// Assert the TRPC error data
+			expect(error.data).toMatchObject({
+				code: 'BAD_REQUEST',
+				httpStatus: 400,
+			});
+
+			// Assert the content of the error message (Zod validation error)
+			const message = JSON.parse(error.message);
+			expect(message[0]).toMatchObject({
+				code: 'custom',
+				message: 'Invalid input',
+				path: ['chainId'],
+			});
+		}
+	});
+});
